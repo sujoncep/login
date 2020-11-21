563d3f54927d0b06971e487f56b230ed34daf0d7
sujoncep <sujoncep@gmail.com>
2020-11-21 21:50:40 +0600

    first commit

---
+++ class3.cs
@@ -0,0 +1,14 @@
+namespace class3
+{
+    
+    class program
+    {
+      string name,city;
+      int age;
+
+      public void AccpetedDetails()
+      {
+          Console.Write("Enter your name");
+      }  
+    }
+}
